b2c7d6eb1dccf800ddd4f7872eba24d296446483
Aditya Anand <72199480+gitteraditya1@users.noreply.github.com>
2021-11-12 21:23:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+
+var randomnumber1=Math.floor(Math.random()*6)+1;
+var randomimagesource1="images/dice"+randomnumber1+".png";
+
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomimagesource1);
+
+
+var randomnumber2=Math.floor(Math.random()*6)+1;
+var randomimagesource2="images/dice"+randomnumber2+".png";
+
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomimagesource2);
+
+if(randomnumber1>randomnumber2){
+  document.querySelector("h1").innerHTML="🚩 Player 1 wins !";
+}
+else if (randomnumber2>randomnumber1){
+  document.querySelector("h1").innerHTML="Player 2 wins ! 🚩"
+}
+else{
+  document.querySelector("h1").innerHTML="Draw";
+}
